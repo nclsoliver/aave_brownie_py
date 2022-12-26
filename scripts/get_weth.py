1efa81e1f62caa593b9ab88bf07d15aafd24cf18
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-12-26 09:21:20 +0100

    update

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -10,5 +10,6 @@ def get_weth():
     account = get_account()
     weth = interface.IWeth(config["networks"] [network.show_active()] ["weth_token"])
     tx = weth.deposit({'from': account, 'value': 0.1 * 10 * 18})
-    print('Received 0.1 WETH')
+    tx.wait(1)
+    print('Received 0.1 WETH')    
     return tx
