309112acf853c32a00a316407a61759fb231fa77
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-12-27 10:53:54 +0100

    fixed some bugs

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -1,15 +1,17 @@
 from scripts.helpful_scripts import get_account
 from brownie import interface, config, network
 
+
 def main():
     get_weth()
 
+
 def get_weth():
-    """Mints WETH by depositing ETH.
-    """
+    """Mints WETH by depositing ETH."""
     account = get_account()
-    weth = interface.IWeth(config["networks"] [network.show_active()] ["weth_token"])
-    tx = weth.deposit({'from': account, 'value': 0.1 * 10 * 18})
+    weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
+    print("test")
+    tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
     tx.wait(1)
-    print('Received 0.1 WETH')    
+    print("Received 0.1 WETH")
     return tx
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,4 +1,4 @@
-from brownie import (network, accounts,config, )
+from brownie import network, accounts,config
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache", "local-ganache", "mainnet-fork", ]
 
