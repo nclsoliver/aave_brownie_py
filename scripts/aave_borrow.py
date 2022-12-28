c683b164cbe929c002320e9e8684fbf58635d1c8
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-12-28 13:33:50 +0100

    fixed some bugs

---
+++ scripts/aave_borrow.py
@@ -0,0 +1,11 @@
+from brownie import network, config
+from scripts.helpful_scripts import get_account
+from scripts.get_weth import get_weth
+
+
+def main():
+    account = get_account()
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        tx = get_weth(account=account)
+        tx.wait(1)
--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -6,12 +6,12 @@ def main():
     get_weth()
 
 
-def get_weth():
+def get_weth(account=None):
     """Mints WETH by depositing ETH."""
     account = get_account()
     weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
     print("test")
-    tx = weth.deposit({"from": account, "value": 0.1 * 10 ** 18})
+    tx = weth.deposit({"from": account, "value": 0.1 * 10**18})
     tx.wait(1)
     print("Received 0.1 WETH")
     return tx
