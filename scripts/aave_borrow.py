df9e02e10232aff0c73fb30590cede3b8147c89b
nclsoliver <96521061+nclsoliver@users.noreply.github.com>
2022-12-30 11:02:10 +0100

    fixed some bugs

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,6 +1,10 @@
 from brownie import network, config, interface
 from scripts.helpful_scripts import get_account
 from scripts.get_weth import get_weth
+from web3 import Web3
+
+# 0.1
+amount = Web3.toWei(0.1, "ether")
 
 
 def main():
@@ -9,11 +13,22 @@ def main():
     if network.show_active() in ["mainnet-fork"]:
         tx = get_weth(account=account)
         tx.wait(1)
-        # ABI
-        # Address
         lending_pool = get_lending_pool()
-        
-        
+        approve_erc20(amount ,lending_pool.address, erc20_address, account)
+        print('Depositing...')
+        tx = lending_pool.deposit(erc20_address, amount, account.address, 0, {'from': account})
+        tx.wait(1)
+        print('Deposited!')
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    print('Approving ERC20 token...')
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print('Approved!')
+    return tx
+    
 
 def get_lending_pool():
     lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider( config["networks"][network.show_active()]["lending_pool_addresses_provider"])
